3ddafc8ecfa1a3119adeac56ed6898a7abf25197
Blake Harrison <blakerh@protonmail.com>
2021-10-08 02:01:25 -0400

    Initial Commit

---
+++ yugioh-db-swiftui/yugioh_db_swiftuiApp.swift
@@ -0,0 +1,18 @@
+//
+//  yugioh_db_swiftuiApp.swift
+//  yugioh-db-swiftui
+//
+//  Created by bhrs on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct yugioh_db_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+}
